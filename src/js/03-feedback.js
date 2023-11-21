fff961b60bba9f4906beb1aa7674ace0bec93085
Igor <igor_socha@yahoo.com>
2023-11-21 21:52:48 +0100

    v 1.2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,9 +1,10 @@
-
 import throttle from 'lodash.throttle';
+
 
 const form = document.querySelector('.feedback-form');
 const emailInput = form.querySelector('input[name="email"]');
 const messageInput = form.querySelector('textarea[name="message"]');
+
 
 // Funkcja zapisuje stan formularza w localStorage
 const saveStateToLocalStorage = throttle(() => {
@@ -12,9 +13,9 @@ const saveStateToLocalStorage = throttle(() => {
     message: messageInput.value,
   };
   localStorage.setItem('feedback-form-state', JSON.stringify(currentState));
-}, 500); // Throttle na 500ms
+}, 500);
 
-// Funkcja wypełnia  formularz danymi z localStorage
+// Funkcja wypełnia formularz danymi z localStorage
 function fillFormFromLocalStorage() {
   const storedState = localStorage.getItem('feedback-form-state');
   if (storedState !== null) {
@@ -27,7 +28,7 @@ function fillFormFromLocalStorage() {
   }
 }
 
-//  wypełnia formularz przy przeładowaniu strony
+// Wypełnij formularz danymi z localStorage po załadowaniu strony
 window.addEventListener('load', fillFormFromLocalStorage);
 
 // Śledzenie zdarzenia input i zapisywanie stanu w localStorage
@@ -38,15 +39,24 @@ form.addEventListener('submit', (event) => {
   event.preventDefault();
   console.log('Form submitted!');
 
-  // Wyczyść localStorage 
-  localStorage.removeItem('feedback-form-state');
-  emailInput.value = '';
-  messageInput.value = '';
-
-  // Wyloguj obecny stan danych z konsoli
+  // Zapisuje obecny stan formularza przed jego wyczyszczeniem
   const currentState = {
     email: emailInput.value,
     message: messageInput.value,
   };
+
+  // Wyczyści localStorage i resetue formularz
+  localStorage.removeItem('feedback-form-state');
+  emailInput.value = '';
+  messageInput.value = '';
+
+  // Pobiera ponownie wprowadzone dane (puste ze względu na resetowanie pól)
+  const loggedState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+
+  // Wyloguje obecny stan formularza
   console.log('Current form state:', currentState);
+  console.log('Logged state:', loggedState);
 });
